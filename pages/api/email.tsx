0bf0a8ae3b8ca3c4451ac81b728a6249e26fa887
Isaiah <isaiahemails@gmail.com>
2024-08-29 06:00:24 +1200

    typescript attempt

--- pages/api/email.tsx
+++ pages/api/email.tsx
@@ -1,45 +1,50 @@
+import { type NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import Mail from "nodemailer/lib/mailer";
 
-export default async function handler(req, res) {
-  if (req.method === "POST") {
-    const { subject, name, email, message } = await req.body;
+export async function POST(request: NextRequest) {
+  const { email, name, subject, message } = await request.json();
 
-    const transport = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.MY_EMAIL,
-        pass: process.env.MY_PASSWORD,
-      },
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    /* 
+      setting service as 'gmail' is same as providing these setings:
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true
+      If you want to use a different email provider other than gmail, you need to provide these manually.
+      Or you can go use these well known services and their settings at
+      https://github.com/nodemailer/nodemailer/blob/master/lib/well-known/services.json
+  */
+    auth: {
+      user: process.env.MY_EMAIL,
+      pass: process.env.MY_PASSWORD,
+    },
+  });
+
+  const mailOptions: Mail.Options = {
+    from: process.env.MY_EMAIL,
+    to: "isaiahemails@gmail.com",
+    // cc: email, (uncomment this line if you want to send a copy to the sender)
+    subject: subject,
+    text: `Message from ${name} (${email})\n${message}`,
+  };
+
+  const sendMailPromise = () =>
+    new Promise<string>((resolve, reject) => {
+      transport.sendMail(mailOptions, function (err) {
+        if (!err) {
+          resolve("Email sent");
+        } else {
+          reject(err.message);
+        }
+      });
     });
 
-    const text = `From ${email} (${name})\n${message}`;
-    const formattedSubject = `Contact form: ${subject}`;
-
-    const mailOptions = {
-      from: process.env.MY_EMAIL,
-      to: "isaiahemails@gmail.com",
-      cc: email,
-      subject: formattedSubject,
-      text: text,
-    };
-
-    const sendMailPromise = () =>
-      new Promise((resolve, reject) =>
-        transport.sendMail(mailOptions, function (err) {
-          if (!err) {
-            resolve("Email sent");
-          } else {
-            reject(err.message);
-          }
-        })
-      );
-    try {
-      await sendMailPromise();
-      res.status(200).json({ message: "Email Sent!" });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
+  try {
+    await sendMailPromise();
+    return NextResponse.json({ message: "Email sent" });
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 500 });
   }
 }
