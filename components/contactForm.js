609bc0f3a07b3cd17348db7cc269a5715605b2bd
Isaiah <isaiahemails@gmail.com>
2024-08-29 07:05:56 +1200

    actual change

--- components/contactForm.js
+++ components/contactForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { sendEmail } from "utils/send_email";
+import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import styles from "../styles/Contact.module.css";
 import { useMediaQuery } from "@mui/material";
@@ -19,9 +19,15 @@ function ContactForm({ initialSubject, ...props }) {
     return () => clearTimeout(timer);
   }, [confirmation]);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
+    // Configure Email.js parameters
+    const templateParams = {
+      subject: subject,
+      user_name: name,
+      user_email: email,
+      message: message,
+    };
     if (subject === "") {
       setconfirmation("Subject cannot be blank");
     } else if (name === "") {
@@ -31,33 +37,31 @@ function ContactForm({ initialSubject, ...props }) {
     } else if (message === "") {
       setconfirmation("Message cannot be blank");
     } else {
-      try {
-        const response = sendEmail({
-          subject: subject,
-          name: name,
-          email: email,
-          message: message,
-        });
-
-        if (response) {
-          setconfirmation(response);
-        } else {
+      // Send the email using Email.js
+      emailjs
+        .send(
+          "service_lx7u87q",
+          "contact_form",
+          templateParams,
+          "eCJbOSpcY59_MuyQm"
+        )
+        .then((response) => {
+          console.log("Email sent!", response.status, response.text);
           setconfirmation("Email sent successfully!");
-        }
-      } catch (error) {
-        console.error("Error sending email:", error);
-        setconfirmation(
-          "There was an error sending your email! Please refresh the page and try again"
-        );
-      }
-
+        })
+        .catch((error) => {
+          console.error("Error sending email:", error);
+          setconfirmation(
+            "There was an error sending your email! Please refresh the page and try again"
+          );
+        });
+      // Clear form fields
       setName("");
       setEmail("");
       setSubject("");
       setMessage("");
     }
   };
-
   return (
     <div>
       <main className="main">
@@ -101,7 +105,7 @@ function ContactForm({ initialSubject, ...props }) {
           </motion.button>
         </form>
         <h1 className="confirmationText">{confirmation}</h1>
-        <h1 className={styles.heading}>Drive to Us</h1>
+        <h1 class={styles.heading}>Drive to Us</h1>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3157.279041196928!2d176.16524927587952!3d-37.689645272007105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d6ddb389053eec7%3A0xf88dad25545ed86!2sGrapplingHQ%20%7C%20Jiu%20Jitsu%20and%20MMA%20gym!5e0!3m2!1sen!2snz!4v1716594990463!5m2!1sen!2snz"
           width={isSmallDevice ? "100%" : "600"}
