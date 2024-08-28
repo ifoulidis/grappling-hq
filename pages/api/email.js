cb5804dbeb344727dbeeb15c3256cb406a26a270
Isaiah <isaiahemails@gmail.com>
2024-08-28 20:34:42 +1200

    email fix

--- pages/api/email.js
+++ pages/api/email.js
@@ -2,6 +2,7 @@ import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
+    console.log(req.body);
     try {
       const { email, name, message, subject } = req.body;
 
