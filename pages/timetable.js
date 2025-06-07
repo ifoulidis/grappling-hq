4c80f1171d03681046083f976ca630a7137986ce
Isaiah Foulidis <isaiahemails@gmail.com>
2025-06-08 07:22:17 +1200

    manually added image

--- pages/timetable.js
+++ pages/timetable.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import TimeTableImage from "@/public/images/timetable.png";
 import { useMediaQuery } from "@mui/material";
-import { CldImage } from "next-cloudinary";
+import Image from "next/image";
 
 function TimeTable({ setPage, ...props }) {
   const isSmallDevice = useMediaQuery("only screen and (max-width : 900px)");
@@ -45,8 +45,8 @@ function TimeTable({ setPage, ...props }) {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <CldImage
-              src={`https://res.cloudinary.com/dvwneza55/image/upload/v1729839674/timetable.png?v=${Date.now()}`}
+            <Image
+              src={TimeTableImage}
               alt="timetable"
               width={1080}
               height={1080}
